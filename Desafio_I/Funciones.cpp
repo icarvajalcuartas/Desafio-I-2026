60d85c4ac9a37d9cfd806800668dd5c9686161a8
icarvajal.cuartas <icarvajal.cuartas@udea.edu.co>
2026-03-17 21:44:01 -0500

    Se creo el puntero para la pieza, donde a partir de un arreglo estatico se genera un numero aleatorio para seleccionar una pieza random

--- Desafio_I/Funciones.cpp
+++ Desafio_I/Funciones.cpp
@@ -1,5 +1,16 @@
 #include "Funciones.h"
-
+#include <cstdlib>
+#include<ctime>
+bool dimensionesValidas(unsigned short alto, unsigned short ancho)
+{
+    if(alto<8||ancho<8){
+        return false;
+    }
+    if(ancho%8 !=0){
+        return false;
+    }
+    return true;
+}
 unsigned char **crearTablero(unsigned short int alto, unsigned short int ancho){
     unsigned short int ancho_bytes=ancho/8;
 
@@ -22,3 +33,36 @@ void liberarTablero(unsigned char**tablero,unsigned short int alto){
     }
     delete [] tablero;
 }
+
+void inciarSemilla()
+{
+    srand(time(0);
+}
+
+void generarPieza(unsigned char *pieza,int )
+{
+    unsigned char piezas[5][4];
+    [[0b00010000,0b00010000,0b00010000,0b00010000],
+    [0b00011000,0b00011000,0b00000000,0b00000000],
+    [0b00001100,0b00011000,0b00000000,0b00000000],
+    [0b00010000,0b00010000,0b00011000,0b00000000],
+    [0b00011100,0b00001000,0b00000000,0b00000000]
+    ]
+
+    int tipo=rand()%5;
+    unsigned char *ptr=*(pieza+tipo);
+    for(int i=0;i<4;i++){
+        *(pieza+i)=*(ptr+i);
+    }
+}
+
+void juego(unsigned char **tablero, unsigned short alto, unsigned short ancho)
+{
+    unsigned char *pieza[4];
+
+    generarPieza(pieza);
+}
+
+
+
+
--- Desafio_I/Funciones.h
+++ Desafio_I/Funciones.h
@@ -1,6 +1,9 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
+bool dimensionesValidas(unsigned short int alto,unsigned short int ancho);
 unsigned char **crearTablero(unsigned short int alto, unsigned short int ancho);
 void liberarTablero(unsigned char **tablero, unsigned short int alto);
+void generarPieza(unsigned char *pieza);
+void juego(unsigned char **tablero, unsigned short alto, unsigned short ancho);
 #endif // FUNCIONES_H
